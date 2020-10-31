ab0dcd42a438c5320f29aed0a8e0893e3ebce983
Gong Biao <developbiao@gmail.com>
2020-10-31 13:44:59 +0800

    Http rpc server and client

---
+++ puzzlers/lesson_11/rpc_client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"os"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "server")
+		os.Exit(1)
+	}
+	serverAddress := os.Args[1]
+
+	// Dial HTTP
+	client, err := rpc.DialHTTP("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	// Synchronous call
+	args := Args{99, 3}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d * %d = %d\n", args.A, args.B, reply)
+
+	var quot Quotient
+	err = client.Call("Arith.Divide", args, &quot)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d / %d = %d remainder %d\n", args.A, args.B, quot.Quo, quot.Rem)
+
+	err = client.Call("Arith.Add", args, &reply)
+	fmt.Printf("Arith: %d + %d = %d\n", args.A, args.B, reply)
+
+}
